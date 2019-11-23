a0a5664ccea0da7ead58a3dec11e9ba52427a036
magk98 <magk98@poczta.fm>
2019-11-23 14:16:58 +0100

    added title screen

---
+++ characterScreen.py
@@ -0,0 +1,52 @@
+from tkinter import *
+import tkinter.font as font
+BG_COLOR = 'grey11'
+BUTTON_COLOR = 'DarkOrange3'
+ON_CLICK_COLOR = 'DarkOrange4'
+
+
+def display():
+    window = Tk()
+    FONT = font.Font(family='Trebuchet MS')
+
+    window.geometry("500x500")
+    window.title("PF spells organizer")
+    window.configure(bg=BG_COLOR)
+    scrollbar = Scrollbar(window)
+    scrollbar.pack(side=RIGHT, fill=Y)
+
+    spell_filter = IntVar()
+
+    level_button = Spinbox(window,
+                           from_=1.0,
+                           to=20.0,
+                           width=5,
+                           )
+    level_button.pack()
+
+    all_spells_button = Radiobutton(window,
+                                    text='lorem ipsum whatever',
+                                    font=FONT,
+                                    indicatoron=0,
+                                    width=20,
+                                    padx=20,
+                                    variable=spell_filter,
+                                    value=1,
+                                    bg=BUTTON_COLOR,
+                                    activebackground=ON_CLICK_COLOR,
+                                    selectcolor=ON_CLICK_COLOR,
+                                    ).pack(anchor=W)
+    char_spells_button = Radiobutton(window,
+                                     text='abc',
+                                     font=FONT,
+                                     indicatoron=0,
+                                     width=20,
+                                     padx=20,
+                                     variable=spell_filter,
+                                     value=2,
+                                     bg=BUTTON_COLOR,
+                                     activebackground=ON_CLICK_COLOR,
+                                     selectcolor=ON_CLICK_COLOR,
+                                     ).pack(anchor=W)
+
+    window.mainloop()
--- titleScreen.py
+++ titleScreen.py
@@ -1,52 +1,78 @@
 from tkinter import *
 import tkinter.font as font
+from PIL import ImageTk, Image
+import os
 BG_COLOR = 'grey11'
 BUTTON_COLOR = 'DarkOrange3'
 ON_CLICK_COLOR = 'DarkOrange4'
+WIDTH, HEIGHT = 550, 550
+BUTTON_Y = 400
+
+def window_init():
+    window = Tk()
+
+    window.geometry("550x550")
+    window.title("PF spells organizer")
+    window.configure(bg=BG_COLOR)
+    return window
+
+def create_char_button(window, FONT, character):
+    char_button = Button(window,
+                         text='Sorcerer lvl.8',
+                         font=FONT,
+                         width=30,
+                         padx=15,
+                         bg=BUTTON_COLOR,
+                         activebackground=ON_CLICK_COLOR,
+                         ).grid(row=1, column=0)
 
 
 def display():
-    window = Tk()
+    #window initialization
+    window = window_init()
     FONT = font.Font(family='Trebuchet MS')
 
-    window.geometry("500x500")
-    window.title("PF spells organizer")
-    window.configure(bg=BG_COLOR)
-    scrollbar = Scrollbar(window)
-    scrollbar.pack(side=RIGHT, fill=Y)
+    frame_main = Frame(window, bg=BG_COLOR)
+    frame_main.grid(sticky='news')
+    # Create a frame for the canvas with non-zero row&column weights
+    frame_canvas = Frame(frame_main)
+    frame_canvas.grid(row=2, column=0, pady=(5, 0), sticky='nw')
+    frame_canvas.grid_rowconfigure(0, weight=1)
+    frame_canvas.grid_columnconfigure(0, weight=1)
+    # Set grid_propagate to False to allow 5-by-5 buttons resizing later
+    frame_canvas.grid_propagate(False)
+
+    scrollbar = Scrollbar(window, orient="vertical")
+    scrollbar.grid(row=1, column=1, sticky='ns')
+    img = ImageTk.PhotoImage(Image.open("title_image.png"))
+    title = Label(window,
+                  image=img,
+                  bg=BG_COLOR,)
+    title.place(x=0, y=10)
+    title.grid(column=0, row=0)
+    # Add a canvas in that frame
+    canvas = Canvas(window, bg=BG_COLOR,  width=WIDTH, height=100, relief=SUNKEN)
+    canvas.grid(row=1, column=0, sticky="news", ipadx=0, ipady=BUTTON_Y)
 
     spell_filter = IntVar()
 
-    level_button = Spinbox(window,
-                           from_=1.0,
-                           to=20.0,
-                           width=5,
-                           )
-    level_button.pack()
+    create_char_button(canvas, FONT, None)
 
-    all_spells_button = Radiobutton(window,
-                                    text='lorem ipsum whatever',
-                                    font=FONT,
-                                    indicatoron=0,
-                                    width=20,
-                                    padx=20,
-                                    variable=spell_filter,
-                                    value=1,
-                                    bg=BUTTON_COLOR,
-                                    activebackground=ON_CLICK_COLOR,
-                                    selectcolor=ON_CLICK_COLOR,
-                                    ).pack(anchor=W)
-    char_spells_button = Radiobutton(window,
-                                     text='abc',
+    edit_button = Button(canvas,
+                                     text='+',
                                      font=FONT,
-                                     indicatoron=0,
-                                     width=20,
-                                     padx=20,
-                                     variable=spell_filter,
-                                     value=2,
+                                     width=5,
+                                     padx=5,
                                      bg=BUTTON_COLOR,
                                      activebackground=ON_CLICK_COLOR,
-                                     selectcolor=ON_CLICK_COLOR,
-                                     ).pack(anchor=W)
+                                     ).grid(row=1, column=1,)
 
+    create_button = Button(canvas,
+                                     text='Create new character',
+                                     font=FONT,
+                                     width=30,
+                                     padx=15,
+                                     bg=BUTTON_COLOR,
+                                     activebackground=ON_CLICK_COLOR,
+                                     ).grid(row=2, column=0,)
     window.mainloop()
