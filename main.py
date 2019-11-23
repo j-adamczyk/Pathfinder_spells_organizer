f432dc7345ea3e01ea8043b5bbb81222ed0040e0
Natalka <natwlo6@wp.pl>
2019-11-23 15:52:40 +0100

    spell_elements

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 
 if __name__ == '__main__':
-    print(12345)
+    text = """Standard
+Move
+Full-round
+Swift
+Immediate
+Free
+"""
+
+    done = text.splitlines()
+    # w = []
+    for d in done:
+        print("\"" + d.lower() + " action\", ", end='')
+        # x = d.split()[0]
+        # if "Domain" not in d:
+        #     if d.lower() not in w:
+        #         w.append(d.lower())
+    # for x in w:
+    #     print("\"" + x + "\", ", end='')
+        # print("\"" + x.lower() + "\", ", end='')
